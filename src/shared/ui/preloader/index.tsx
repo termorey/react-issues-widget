5c21b62138fb334152fcddd0b929a2599f4fb417
termorey <is_front@mail.ru>
2023-08-12 02:03:30 +0300

    feature: added animated variant

--- src/shared/ui/preloader/index.tsx
+++ src/shared/ui/preloader/index.tsx
@@ -1,9 +1,12 @@
+import { a } from "@react-spring/web";
 import style from "./style.module.scss";
 
-export const Preloader = () => (
-	<div className={style.preloader}>
+export const Preloader = ({ ...props }) => (
+	<div className={style.preloader} {...props}>
 		<span />
 		<span />
 		<span />
 	</div>
 );
+
+export const AnimatedPreloader = a(Preloader);
