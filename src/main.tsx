d556af9bbfd8c9f3e1035277101dd0801185ef1c
termorey <is_front@mail.ru>
2023-08-12 18:54:12 +0300

    feature: added mock server, added links from widget

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
+import type { Links } from "shared/config/links";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./app";
+import { App } from "app";
+import { isDev } from "shared/config/environment.ts";
+import { linksApi } from "shared/config/links";
 
 declare global {
 	interface Window {
@@ -8,6 +11,10 @@ declare global {
 			IssueWidget: Widget;
 		};
 	}
+}
+interface Options {
+	element: HTMLElement;
+	links: Links;
 }
 
 const create: (options: { element: HTMLElement }) => void = ({ element }) =>
@@ -18,15 +25,27 @@ const create: (options: { element: HTMLElement }) => void = ({ element }) =>
 	);
 
 export class Widget {
-	constructor({ element }: { element: HTMLElement }) {
+	constructor({ element, links }: Options) {
+		linksApi.setLinks(links);
 		create({ element });
 	}
 }
 
-window.widgets = { ...window.widgets, IssueWidget: Widget };
+const start = async () => {
+	window.widgets = { ...window.widgets, IssueWidget: Widget };
 
-if (process.env.NODE_ENV?.trim() === "development") {
-	const element = document.getElementById("root");
-	if (element) new Widget({ element });
-	else throw Error("Undefined target element");
-}
+	if (isDev) {
+		const { startMockServer } = await import("./mocks/browser");
+		console.log("TEST");
+		const element = document.getElementById("root");
+		const links: Links = {
+			issues: "/api/issues",
+			issuesStatuses: "/api/issues/statuses",
+		};
+		startMockServer();
+		if (element) new Widget({ element, links });
+		else throw Error("Undefined target element");
+	}
+};
+
+start().catch((e) => console.log(e));
